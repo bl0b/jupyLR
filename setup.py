01de62592160f8a72ae48d6b9099c73facc59165
Damien Leroux <damien.leroux@gmail.com>
2011-12-15 21:23:49 +0100

    Seems this file has been missing forever.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jupyLR',
+      version='0.1',
+      description='GLR parsing for python with on-line ' +
+                  'AST validation and basic error reporting.',
+      author='Damien "bl0b" Leroux',
+      author_email='damien.leroux@gmail.com',
+      url='http://bl0b.github.com/jupyLR/',
+      packages=['jupyLR'],
+     )
